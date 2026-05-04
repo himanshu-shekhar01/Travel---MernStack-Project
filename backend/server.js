78501398de1614387ef2ef51e75f616f111680e5
himanshu-bro <himanshushekhar9899@gmail.com>
2026-05-04 11:05:46 +0530

    fix preflight cors

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://travel-mern-stack-project-mu.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
